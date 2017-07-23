761e27f9a620e330094ace50f755f743a022dbaf
CH196308 <carlharrison@carls-mbp.home>
2017-07-23 17:53:47 +0100

    First Commit
    Added Unity project to GitHub

---
+++ Fudogotchi_v1/Assets/Scripts/onWaterClick.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class onWaterClick : MonoBehaviour {
+
+	public Button waterButton;
+
+	public GameObject waterAnimation;
+	public GameObject wateringCan;
+	public GameObject waterHighlight;
+
+	Animator canAnimation;
+	Animator waterHighlightAnim;
+
+
+	void Start()
+	{
+		canAnimation = wateringCan.GetComponent<Animator> ();
+		waterHighlightAnim = waterHighlight.GetComponent<Animator> ();
+		Button btn = waterButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+		wateringCan.SetActive (false);
+	}
+
+	public void TaskOnClick()
+	{
+		if(GameObject.Find("Tomato").GetComponent<TomatoManager>().clickedTomato)
+		{
+			waterHighlight.SetActive (true);
+			wateringCan.SetActive (true);
+			canAnimation.Play ("CanPour");
+			waterAnimation.SetActive (true);
+			waterHighlightAnim.Play ("WaterHighlight");
+
+			StartCoroutine (StopWater ());
+
+			GameObject.Find ("Tomato").GetComponent<TomatoManager> ().updateThirst (1);
+		} 
+	}
+
+	IEnumerator StopWater()
+	{
+		yield return new WaitForSeconds (2.0f);
+		waterHighlight.SetActive (false);
+		wateringCan.SetActive (false);
+		waterAnimation.SetActive (false);
+	}
+}
