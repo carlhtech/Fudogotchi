95e551db32ca01c3a1f6f65ca2be359ca863cd2a
Phillip Penny <phillippenny@gmail.com>
2017-07-24 16:59:14 +0100

    added in old version of Unity project

---
+++ Fudogotchi_v1/Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public Vector3 Rotation = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Rotation * Time.deltaTime);
+	}
+}
